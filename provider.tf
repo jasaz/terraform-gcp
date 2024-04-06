c35b517d5278c46a06e3804d639342311e755219
jasaz <jasaz@users.noreply.github.com>
2024-04-06 23:35:06 +0530

    Update provider.tf
    updated the required version

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
   
-  required_version="~>1.1.17"
+  required_version=">1.1.17"
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "~> 5.18"
+      version = "> 5.18"
     }
   }
 

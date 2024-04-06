701f1b21fbb87a3788ac0fabccd4fd87f06afbfb
jasaz <jasaz@users.noreply.github.com>
2024-04-07 00:02:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "region" {
   type = string
   description = "GCP Project Region"
 }
-
-variable "tfstate_bucket" {
-  type = string
-  description = "Bucket where tfstate is stored"
-}
-
-variable "tfstate_prefix" {
-  type = string
-  description = "Object where tfstate is stored"
-}

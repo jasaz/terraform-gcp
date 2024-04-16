0bfdc397614b47ab751ad0326f841f18dd926b8f
jasaz <sandip.pradhan@gmail.com>
2024-04-16 22:13:47 +0530

    cloud run variables

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,4 @@
 project_id = "cloudrun-test-415115"
 region = "asia-south1"
+cloud_run_sa_name = "custom-cr-sa-1"
+cloud_run_ingress = "INGRESS_TRAFFIC_ALL"
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "region" {
   type = string
   description = "GCP Project Region"
 }
+
+variable "cloud_run_sa_name" {
+  type = string
+  description = "Cloud Run Service Account"
+}
+
+variable "cloud_run_ingress" {
+  type = string
+  description = "Cloud Run Ingress setting"
+}

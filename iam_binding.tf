7f2b62aef00d9638cd754f36c7a92553ab60ddf7
jasaz <sandip.pradhan@gmail.com>
2024-04-23 21:19:05 +0530

    fixed a bug

--- iam_binding.tf
+++ iam_binding.tf
@@ -3,6 +3,7 @@ resource "google_service_account_iam_binding" "cr-sa-github-wi-sauser-binding" {
     role = "roles/iam.serviceAccountUser"
 
     members = [
-        "serviceAccount:google_service_account.cloud_run_sa.email",
+        "serviceAccount:${google_service_account.cloud_run_sa.email}",
+        
     ]
 }

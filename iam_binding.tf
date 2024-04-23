df1b33ceb4ed0f2376285fa3ed897e4bfb4ee3fc
jasaz <sandip.pradhan@gmail.com>
2024-04-23 22:19:17 +0530

    Updated IAM binding block

--- iam_binding.tf
+++ iam_binding.tf
@@ -1,9 +1,9 @@
 resource "google_service_account_iam_binding" "cr-sa-github-wi-sauser-binding" {
-    service_account_id = data.google_service_account.workload-identifier-sa.name
+    service_account_id = google_service_account.cloud_run_sa.email
     role = "roles/iam.serviceAccountUser"
 
     members = [
-        "serviceAccount:${google_service_account.cloud_run_sa.email}",
+        "serviceAccount:${data.google_service_account.workload-identifier-sa.name}",
         
     ]
 }

eec0ecf6450b6682e5d69b8cfc8a8e788fa14d4a
jasaz <sandip.pradhan@gmail.com>
2024-04-23 15:49:38 +0530

    Fixed bug for variable github_wi_sa

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
-locals {
-    github_wi_sa =  "github-workload-identifier-sa"@${var.project_id}.iam.gserviceaccount.com
 
+locals {
+    github_wi_sa =  "github-workload-identifier-sa@${var.project_id}.iam.gserviceaccount.com"
 }
 
 data "google_service_account" "workload-identifier-sa" {

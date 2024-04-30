b695a52c8da7d46f9139d76fa6ccdc6e9bfcd907
jasaz <sandip.pradhan@gmail.com>
2024-04-30 22:59:55 +0530

    .\cloud-run.tf

--- iam_binding.tf
+++ iam_binding.tf
@@ -1,5 +1,7 @@
+/*
 resource "google_service_account_iam_member" "cr-sa-github-wi-sa-binding" {
     service_account_id = google_service_account.cloud_run_sa.name
     role = "roles/iam.serviceAccountUser"
     member = "serviceAccount:${data.google_service_account.workload-identifier-sa.email}"
 }
+*/

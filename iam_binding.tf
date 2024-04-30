1c7fedc66e278a1d96da56665d5df3d31bca45f1
jasaz <sandip.pradhan@gmail.com>
2024-04-30 22:35:31 +0530

    bux fix

--- iam_binding.tf
+++ iam_binding.tf
@@ -1,4 +1,4 @@
-resource "google_service_account_iam_member" "cr-sa-github-wi-sauser-binding" {
+resource "google_service_account_iam_member" "cr-sa-github-wi-sa-binding" {
     service_account_id = google_service_account.cloud_run_sa.name
     role = "roles/iam.serviceAccountUser"
     member = "serviceAccount:${data.google_service_account.workload-identifier-sa.email}"
